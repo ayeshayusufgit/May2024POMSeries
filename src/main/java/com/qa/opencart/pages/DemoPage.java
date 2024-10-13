6bc1be2cf4802a09f068bba8907e485e8a084757
ayeshayusufgit <ayesha.yusuf@gmail.com>
2024-10-13 23:42:13 +0530

    added DemoPage

---
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+
+	By loc=By.id("demo");
+	
+	public void getPage() {
+		System.out.println("Click on Demo");
+	}
+}
