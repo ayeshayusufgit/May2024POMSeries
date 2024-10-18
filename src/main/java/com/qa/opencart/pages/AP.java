3c03c385d88e3c2bc65f73ca7a818a9168c8c698
ayeshayusufgit <ayesha.yusuf@gmail.com>
2024-10-18 19:37:19 +0530

    Added tbe code once again

---
+++ src/main/java/com/qa/opencart/pages/AP.java
@@ -0,0 +1,7 @@
+package com.qa.opencart.pages;
+
+public class AP {
+	int i = 10;
+	int y = 20;
+
+}
--- src/main/java/com/qa/opencart/pages/TestPage.java
+++
@@ -1,9 +0,0 @@
-public class TestPage {
-
-	By loc=By.cssSelector(".test");
-	
-	public int getPage() {
-		System.out.println("Click on Test");
-		return 0;
-	}
-}
